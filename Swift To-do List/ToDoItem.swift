2bcc7201faa14ad979cbfbe0b719eeb366acc93a
pahtz <paul@Pauls-iMac.local>
2014-06-17 19:55:48 +0800

    Completed
    Rewritten in Swift

---
+++ Swift To-do List/ToDoItem.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoItem.swift
+//  Swift To-do List
+//
+//  Created by Paul Yu on 13/6/14.
+//  Copyright (c) 2014 Paul Yu. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItem: NSObject {
+    var itemName : String = ""
+    var completed : Bool = false
+    var creationDate : NSDate = NSDate()
+    
+    
+}
---
+++ Swift To-do List/ToDoListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ToDoListTableViewController.h
+//  Swift To-do List
+//
+//  Created by Paul Yu on 13/6/14.
+//  Copyright (c) 2014 Paul Yu. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+
+@interface ToDoListTableViewController
+- (IBAction)unwindToList: (UIStoryboardSegue *)segue;
+@end
